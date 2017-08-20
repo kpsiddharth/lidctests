d11cc19d24fe0bd4f2d23580d362d48d2116b5f7
aman agrawal <agrawal.aman27@gmail.com>
2017-08-21 01:42:17 +0530

    preparing all nodule data on which we are going to train and test that a nodule is malignant or not using CNN

---
+++ viewer/data.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pylidc as pl
+#from sqlalchemy import or_
+
+#Collecting all annotation object.
+annotations = pl.query(pl.Annotation)
+
+#Taking only 3 annotations-If need to check data
+annotations = qu.filter(or_(pl.Annotation.scan_id == 14, pl.Annotation.scan_id == 7,pl.Annotation.scan_id == 10))
+
+#Counting Total annotation/Nodule
+annotations_count = annotations.count()
+
+#Creating numpy array for nodule data.
+#It will be used as training input data in CNN.
+resampled_nodule_data = np.zeros((annotations_count,40,40,40))
+
+#Creating numpy array for maligancy of each nodule.
+#It will be used as target data in CNN.
+target_data = np.zeros(annotations_count)
+
+#Updating nodule and target data with actual value 
+#by iterating on each annotation/nodule.
+for count, annotation in enumerate(annotations):
+    ann_vol, ann_seg = annotation.uniform_cubic_resample(side_length = 39)
+    resampled_nodule_data[count] = ann_vol
+    target_data[count] = annotation.malignancy
+
+#Converting 4D nodule data into 2D.
+#As we can't save nD(n>2) array as it is into file.
+#For this either we will have to reshape this array into 1D or 2D array.
+#or will have to save in binary form.
+resampled_nodule_data_in_2D = np.reshape(resampled_nodule_data,(annotations_count,40*40*40))
+
+#Converting array data type float64 to int8.
+resampled_nodule_data_in_2D_int8 = resampled_nodule_data_in_2D.astype(np.int8)
+
+#storing nodule data and target data in CSV format.
+np.savetxt("final_data.csv", resampled_nodule_data_in_2D_int8, delimiter=",")
+np.savetxt("target_data.csv", target_data, delimiter=",")
