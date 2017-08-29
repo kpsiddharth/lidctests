53cf0be817f408647790bcd64daeb76deeb991b8
Ubuntu <ubuntu@ip-172-31-17-28.ap-southeast-1.compute.internal>
2017-08-29 03:50:36 +0000

    Adding data preprocessing and minor changes to NN architecture

--- mlp/FeedForwardLIDCModel.py
+++ mlp/FeedForwardLIDCModel.py
@@ -6,6 +6,8 @@ Created on Aug 23, 2017
 
 import numpy as np
 from sklearn.neural_network import MLPClassifier
+from sklearn import preprocessing
+
 
 train_data = 'data1500/final_data_1.csv'
 result_data = 'data1500/target_data_1.csv'
@@ -15,6 +17,7 @@ target_data = []
 
 data_size = 1500
 num_classes = 6
+num_iter = 500 
 
 def load_data():
     print ('Loading Input Data ..')
@@ -31,6 +34,9 @@ def load_data():
     for j in i:
         new_target_data[j, target_data[j]] = 1
 
+    print ('Sample Data = ', training_data[0][0:30])
+    print ('Sample Data = ', training_data[10][0:30])
+
     print ('New Target Data = \n\n', new_target_data)
 
     #cdata = np.concatenate((training_data, target_data), axis = 1)
@@ -41,18 +47,41 @@ def load_data():
     print ('Training Data Shape = ', np.shape(training_data))
     print ('Training Results Shape = ', np.shape(target_data))
     
+    training_data = training_data.astype(type(np.inf))
+    target_data = target_data.astype(type(np.inf))
+
+    min_max_scaler = preprocessing.MinMaxScaler()
+    training_data = min_max_scaler.fit_transform(training_data)
+
+    print ('Training Data = ', training_data[0][0:30])
+    print ('Training Data = ', training_data[10][0:30])
+
+
+    print ('Training Data Shape = ', np.shape(training_data))
+
     return training_data, new_target_data
 
 def build_mlp_classifier(training_data, target_data):
     print ('Building MLP Classifier .. ')
-    #mlp = MLPClassifier(solver='lbfgs', max_iter=1000, validation_fraction=0.1, alpha=1e-5, hidden_layer_sizes=(1024, 128, 128, 64, 6))
-    mlp = MLPClassifier(solver='adam', verbose=True, max_iter=10000, validation_fraction=0.1, alpha=1e-5, hidden_layer_sizes=(1024, 1024, 256, 128, 6))
+    mlp = MLPClassifier(solver='lbfgs', max_iter=num_iter, validation_fraction=0.1, alpha=1e-5, hidden_layer_sizes=(512, 256, 64, 6))
+    #mlp = MLPClassifier(solver='adam', verbose=True, max_iter=num_iter, validation_fraction=0.1, alpha=1e-5, hidden_layer_sizes=(1024, 1024, 256, 128, 6))
     print ('Training Data Shape = ', np.shape(training_data))
+    print ('Target Data Shape = ', np.shape(target_data))
+
+    # print ('Number of Outputs = ', mlp.n_outputs)
+
     mlp.fit(training_data, target_data)
     print ('Built Classifier .. ')
 
-    predictions = mlp.predict(training_data)
+    predictions = mlp.predict_proba(training_data)
     i = range(len(training_data))
+
+    #print ('Number of Outputs = ', mlp.n_outputs)
+
+    weights = mlp.get_params()
+    print ('Weights Shape = ', np.shape(weights))
+
+    print ('Predictions Shape = ', np.shape(predictions))
 
     print ('\n\nBelow are the actual comparisons for the performance of the ANN versus the actual target values .. ')
 
@@ -64,7 +93,7 @@ def build_mlp_classifier(training_data, target_data):
     for j in i:
         predicted_value = np.argmax(predictions[j])
         target_value = np.argmax(target_data[j])
-        print ('Prediction = ', predicted_value, ' Target = ', target_value)
+        print ('', predictions[j], ' :: Prediction = ', predicted_value, ' Target = ', target_value)
         if predicted_value == target_value:
             correct_predictions = correct_predictions + 1
         else:
