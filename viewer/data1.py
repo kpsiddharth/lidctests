85f7273496c1eb3230370082fbe8a0355fd0d6ee
aman agrawal <agrawal.aman27@gmail.com>
2017-09-05 03:56:07 +0530

    First version data preprocessing

--- viewer/data1.py
+++ viewer/data1.py
@@ -9,7 +9,7 @@ uside_length = 25
 annotations = pl.query(pl.Annotation)
 
 #Taking only 3 annotations-If need to check data
-#annotations = annotations.filter(or_(pl.Annotation.scan_id == 17, pl.Annotation.scan_id == 27,pl.Annotation.scan_id == 10))
+#annotations = annotations.filter(or_(pl.Annotation.scan_id == 7, pl.Annotation.scan_id == 14,pl.Annotation.scan_id == 10))
 
 #Counting Total annotation/Nodule
 annotations_count = annotations.count()
@@ -21,7 +21,7 @@ resampled_nodule_data = np.zeros((annotations_count,uside_length,uside_length,us
 
 #Creating numpy array for maligancy of each nodule.
 #It will be used as target data in CNN.
-target_data = np.zeros((annotations_count, 2))
+target_data = np.zeros((annotations_count, 4))
 
 counter = 0
 
@@ -38,7 +38,7 @@ for count, annotation in enumerate(annotations):
             continue
         print 'Annotation ID = ', annotation.id, ' : Processing .. Malignancy = ', annotation.malignancy
         ann_vol, ann_seg = annotation.uniform_cubic_resample(side_length = (uside_length-1))
-        resampled_nodule_data[count] = ann_vol
+        resampled_nodule_data[counter] = ann_vol
         rows, cols, heights = np.where(ann_seg == False)
         mask = np.dstack((rows, cols, heights))
         sh = np.shape(mask)
@@ -47,33 +47,48 @@ for count, annotation in enumerate(annotations):
         i = 0
         for ind in mask:
             i = i + 1
-        #    print 'Setting (', ind[0], ',', ind[1], ',', ind[2], ') to 0 ..'
-        #    ann_vol[ind[0]][ind[1]][ind[2]] = 0
-        print 'i = ', i
-        print '\n\nAnn Vol = ', np.shape(ann_vol)
-        target_data[count][0] = annotation.malignancy
-        target_data[count][1] = annotation.estimate_diameter()
+            #print 'Setting (', ind[0], ',', ind[1], ',', ind[2], ') to 0 ..'
+            ann_vol[ind[0]][ind[1]][ind[2]] = 0
+        #print 'i = ', i
+        #print '\n\nAnn Vol = ', np.shape(ann_vol)
+        print counter, "---type---", type(annotation.malignancy),"annotation_malignancy", annotation.malignancy
+        if annotation.malignancy < 3:
+            target_data[counter][0] = 1
+        elif annotation.malignancy == 3:
+            target_data[counter][1] = 1
+        else:
+            target_data[counter][2] = 1
+        target_data[counter][3] = annotation.estimate_diameter()
+
+        counter = counter+1
+        print ('Counter = ', counter)
+        if counter > annotations_count:
+            break
     except Exception as e:
         print (e.message, annotation.scan_id)
 
-    counter = counter+1
-    print ('Counter = ', counter)
-    if counter > annotations_count:
-        break
 
-resampled_nodule_data_in_2D = np.delete(resampled_nodule_data_in_2D, np.s_[i:annotation_count])
-print 'Resized the Resampled Array to = ', np.shape(resampled_nodule_data_in_2D)
+resampled_nodule_data = resampled_nodule_data[:counter]
+target_data = target_data[:counter]
+print 'Resampled Array = ', np.shape(resampled_nodule_data)
 
 #Converting 4D nodule data into 2D.
 #As we can't save nD(n>2) array as it is into file.
 #For this either we will have to reshape this array into 1D or 2D array.
 #or will have to save in binary form.
-resampled_nodule_data_in_2D = np.reshape(resampled_nodule_data,(np.shape(resampled_nodule_data_in_2D)[0],uside_length*uside_length*uside_length))
+resampled_nodule_data_in_2D = np.reshape(resampled_nodule_data,(np.shape(resampled_nodule_data)[0],uside_length*uside_length*uside_length))
 print 'Resized Resample Array to 2D = ', np.shape(resampled_nodule_data_in_2D)
 
 #Converting array data type float64 to int8.
 
+#resampled_nodule_data_in_2D[0][729:781]
+
+#Feature Scaling
+scaled_training_data = (resampled_nodule_data_in_2D)/((np.amax(resampled_nodule_data_in_2D) - np.amin(resampled_nodule_data_in_2D)) * 1.0)
+
+
 #storing nodule data and target data in CSV format.
-np.savez('training_data.npz', data = resampled_nodule_data_in_2D)
+np.savez('unscaled_training_data.npz', data = resampled_nodule_data_in_2D)
+np.savez('training_data.npz', data = scaled_training_data)
 np.savez('training_targets.npz', data = target_data)
 np.savez('bbox_sizes.npz', data = bbox_size)
