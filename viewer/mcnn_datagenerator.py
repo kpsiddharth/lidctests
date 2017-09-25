f689374d0034f40b95dc26ab15388e9054183411
Siddhartha Purkayastha <siddhartha@wiseswan.com>
2017-09-25 20:34:08 +0530

    Changes to create and apply masks for multiple layers. Also temporary changes to outline nodule boundaries.

--- viewer/mcnn_datagenerator.py
+++ viewer/mcnn_datagenerator.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pylidc as pl
+import dicom
 from sqlalchemy import or_
 from skimage import draw
+import math, os, sys
+import pylab
 
-STANDARD_SHAPE = (32, 32)
+# Path to DICOM images
+DICOM_PATH = ''
 
-def create_mask(coordinates, shape = STANDARD_SHAPE):
+# Standard CT scan size. This will
+# be needed to build the mask.
+STANDARD_SHAPE = (512, 512)
+
+def create_mask(coordinates, shape=STANDARD_SHAPE):
     '''
     `coordinates` as represented in the Pylidc database for
     outlining the boundary of individual nodules.
@@ -15,28 +23,44 @@ def create_mask(coordinates, shape = STANDARD_SHAPE):
     rows = [int(c[0]) for c in coords_split]
     cols = [int(c[1]) for c in coords_split]
 
-    min_x = np.amin(rows)
-    min_y = np.amin(cols)
+    # min_x = np.amin(rows)
+    # min_y = np.amin(cols)
     
-    rows = [r - min_x for r in rows]
-    cols = [c - min_y for c in cols]
+    # rows = [r - min_x for r in rows]
+    # cols = [c - min_y for c in cols]
     
     rows, cols = draw.polygon(rows, cols, shape)
     
-    mask = np.zeros(shape, dtype = np.bool)
+    mask = np.zeros(shape, dtype=np.bool)
     mask[rows, cols] = True
     
     return mask
 
-def get_middle_contours(contours):
+def get_dicom_file_name_adjustments(file_name):
+    file_name = file_name.rjust(10, '0')
+    return file_name
+
+def get_middle_contours(contours, base_path):
     '''
     This function returns the middle 
     'n' contours from the set of available
     contours in the DB.
     '''
+    if True:
+        return contours
+    
+    print ('Contours Available = ', len(contours))
+    
     incl_contours = []
     for c in contours:
-        if c.inclusion:
+        file_name = c.dicom_file_name
+        file_name = get_dicom_file_name_adjustments(file_name)
+        file_path = base_path + '/' + file_name
+        
+        # Ensure that the contour is an inclusion contour
+        # and that the corresponding DICOM file path actually 
+        # exists in the File System DICOM downloads
+        if c.inclusion and os.path.exists(file_path):
             incl_contours.append(c)
     
     contours = incl_contours
@@ -53,7 +77,7 @@ def get_middle_contours(contours):
     if length == 3:
         return contours
 
-    mid_point = length / 2
+    mid_point = int(math.floor(length / 2))
     mid_contours = []
     
     for i in range(mid_point - 1, mid_point + 2):
@@ -61,30 +85,112 @@ def get_middle_contours(contours):
     
     return mid_contours
 
-annotations = pl.query(pl.Annotation)
+annotations = pl.query(pl.Annotation).filter(pl.Annotation.id == 39)
 annotations_count = annotations.count()
 
 qualified_ann_count = 0
 
+max_xrange = 0
+max_yrange = 0
+min_xrange = 100000
+min_yrange = 100000
+
 for ann in annotations:
+    ann_id = str(ann.id)
+    ann_id = ann_id.rjust(8, ' ')
+    sys.stdout.flush()
+    print ('Processing Annotation ID = ' + ann_id)
     scan = ann.scan
     contours = ann.contours
+    
     if len(contours) > 2:
         '''
         These are the annotations that should figure in the final 
         generated data for training and testing of the model.
         '''
         qualified_ann_count += 1
-        # print ('Contour [0] = ', contours[0].id)
         sorted_contours = sorted(contours, key=lambda c: c.image_z_position)
-        mid_contours = get_middle_contours(sorted_contours)
+        base_path = scan.get_path_to_dicom_files(checkpath=False)
+        mid_contours = get_middle_contours(sorted_contours, base_path)
+        if not (os.path.exists(base_path)):
+            continue
         
-        if len(mid_contours) == 3:
+        if len(mid_contours) >= 3:
+            # Once we have all the contours, we will recreate
+            # masks and then apply them to original DICOM images
             for contour in mid_contours:
                 ctr_coords = contour.to_matrix()
-                # print ('File Name = ', contour.dicom_file_name, ' \nCoords = ', ctr_coords)
+                xmax = np.amax(ctr_coords[:, 0])
+                xmin = np.amin(ctr_coords[:, 0])
+                ymax = np.amax(ctr_coords[:, 1])
+                ymin = np.amin(ctr_coords[:, 1])
+                
+                xrange = (xmax - xmin)
+                yrange = (ymax - ymin)
+                
+                if(xrange > max_xrange):
+                    max_xrange = xrange
+                    
+                if(yrange > max_yrange):
+                    max_yrange = yrange
+                    
+                if(xrange < min_xrange):
+                    min_xrange = xrange
+                
+                if(yrange < min_yrange):
+                    min_yrange = yrange
+                
+                # print ('    X Range = %d', (xmax - xmin))
+                # print ('    Y Range = %d', (ymax - ymin))
+                
+                # (xcentroid, ycentroid) is the center of the
+                # ROI from which the nodule will be extracted
+                # into a square of 32x32 pixels
+                xcentroid = (xmin + xmax) / 2
+                ycentroid = (ymin + ymax) / 2
+                
+                mask = create_mask(contour.coords)
+                file_name = contour.dicom_file_name
+                
+                # Adjust the filename to fit to the LIDC 
+                # 10 character format
+                file_name = file_name.rjust(10, '0')
+                
+                # Now building the file path to load the 
+                # DCM image and do the needful processing
+                file_path = base_path + '/' + file_name
+                ds = dicom.read_file(file_path)
+                pixel_array = np.copy(ds.pixel_array)
+
+                rows, cols = np.where(mask == False)
+                mask = np.dstack((rows, cols))
+                sh = np.shape(mask)
+                mask = np.reshape(mask, (sh[1], sh[2]))
+                for ind in mask:
+                    pixel_array[ind[0]][ind[1]] = 0
+
+                side_by_side = np.concatenate((pixel_array, ds.pixel_array), axis=1)
+                side_by_side = np.concatenate((pixel_array, ds.pixel_array), axis=1)
+                
+                pylab.imshow(ds.pixel_array, cmap=pylab.cm.bone)
+                
+                print ('File = ', file_path)
+                print ('Contour Id = ', contour.id)
+                print ('Annotation = ', ann.id)
+                print ('Z Index = ', contour.image_z_position)
+                
+                # red_rows = [512 + x for x in ctr_coords[:,0]]
+                red_rows = ctr_coords[:,0]
+                red_cols = ctr_coords[:,1]
+                pylab.plot(red_cols, red_rows, 'r')
+                
+                pylab.show()
         else:
             print ('Skipping Annotation ', ann.id, ' as not enough contours found ..')
+
+print ('Maximum X Range = %d, Y Range = %d', max_xrange, max_yrange)
+print ('Minimum X Range = %d, Y Range = %d', min_xrange, min_yrange)
+
 
 print ('Total Annotations = ', annotations_count)
 print ('Qualified Annotations = ', qualified_ann_count)
